f4b68a394592e6c8e952dc9d5da7b49494deaf21
karon <karon@yahoo.com>
2015-05-17 13:51:14 +0800

    Volley: import A1OS_API project .

---
+++ src/com/a1os/cloud/phone/DBHelper.java
@@ -0,0 +1,24 @@
+package com.a1os.cloud.phone;
+ 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+ 
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "TestSQLite";
+    public static final int VERSION = 1;
+
+    public StuDBHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table cloud_table(phone varchar(20),location varchar(20))";
+        db.execSQL(sql);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+}  
---
+++ src/com/a1os/cloud/phone/PhoneUtil.java
@@ -0,0 +1,73 @@
+package com.a1os.cloud.phone;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public final class PhoneUtil {
+
+    static String A1OS_API = "http://www.aoidone.com/api/cloud_data.php?number=";
+
+    static StringBuilder resultBuilder = new StringBuilder();
+    
+    static DBHelper dbHelper;
+    static SQLiteDatabase db;
+
+    public static void getNumberInfo(Context ctx, final String phoneNumber, final CallBack callBack) {
+
+        final String PHONENUMBER_COMPLETE = phoneNumber.replaceAll("(?:-| )", "");
+        final String A1OS_URL = A1OS_API + PHONENUMBER_COMPLETE;
+        
+        dbHelper = new StuDBHelper(ctx,"cloud_db",null,1);
+        db = dbHelper.getReadableDatabase();
+        
+        if (query(PHONENUMBER_COMPLETE) != null) {
+            callBack.execute(query(PHONENUMBER_COMPLETE));
+            db.close();
+            return;
+        }
+
+        RequestQueue mQueue = Volley.newRequestQueue(ctx);
+
+        StringRequest stringRequest = new StringRequest(A1OS_URL,
+           new Response.Listener<String>() {
+              @Override
+              public void onResponse(String response) {
+            	  callBack.execute(response);
+            	  ContentValues cv = new ContentValues();
+                  cv.put("phone", PHONENUMBER_COMPLETE);
+                  cv.put("location", response);
+                  db.insert("cloud_table", null, cv);
+                  db.close();
+              }
+        }, new Response.ErrorListener() {
+              @Override
+              public void onErrorResponse(VolleyError error) {
+                  Log.e("TAG", error.getMessage(), error);
+              }
+        });
+        mQueue.add(stringRequest);
+    }
+
+    public static String query(String phonen) {
+    	Cursor cursor = db.query("cloud_table", new String[]{"phone","location"},null,null, null, null, null);
+    	while(cursor.moveToNext()){
+              if (phonen.equals(cursor.getString(cursor.getColumnIndex("phone")))) {
+                  return cursor.getString(cursor.getColumnIndex("location"));
+              }
+        }
+        return null;
+    }
+
+    public interface CallBack {
+        void execute(String response);
+    }
+}
