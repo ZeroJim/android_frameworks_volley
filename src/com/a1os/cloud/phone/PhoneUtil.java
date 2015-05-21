381612a25e6ea4356f5af0043e79ec5a00d1642a
karon <karon@yahoo.com>
2015-05-21 21:19:05 +0800

    A1OS: add cloud a1os upload API .

--- src/com/a1os/cloud/phone/PhoneUtil.java
+++ src/com/a1os/cloud/phone/PhoneUtil.java
@@ -1,13 +1,17 @@
 package com.a1os.cloud.phone;
 
+import java.util.HashMap;
+
 import com.a1os.cloud.request.APIRequest;
 import com.a1os.cloud.util.L1Cache;
 import com.a1os.cloud.util.L2Cache;
 import com.a1os.cloud.util.StringCache;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.Request.Method;
 import com.android.volley.toolbox.Volley;
 
 import android.content.Context;
@@ -16,8 +20,9 @@ import android.util.Log;
 public final class PhoneUtil {
 
     static String A1OS_API = "http://www.aoidone.com/api/cloud_data.php?number=";
+    static String A1OS_API2 = "http://www.aoidone.com/api/upload.php";
 
-    static L1Cache<String, String> mL1Cache = new L1Cache<String, String>(2 * 1024 *1024);
+    static L1Cache<String, String> mL1Cache = new L1Cache<String, String>(2 * 1024 * 1024);
     static StringCache mL2Cache;
 
     public static void getNumberInfo(Context ctx, final String phoneNumber, final CallBack callBack) {
@@ -51,6 +56,35 @@ public final class PhoneUtil {
               }
         });
         mQueue.add(stringRequest);
+    }
+
+    public static void putNumberInfo(Context ctx, final String phoneNumber, final String tag) {
+
+        final String PHONENUMBER_COMPLETE = phoneNumber.replaceAll("(?:-| )", "");
+
+        RequestQueue mQueue = Volley.newRequestQueue(ctx);
+
+        APIRequest PostRequest = new APIRequest(Method.POST, A1OS_API2,
+           new Response.Listener<String>() {
+              @Override
+              public void onResponse(String response) {
+                  Log.e("TAG", response);
+              }
+        }, new Response.ErrorListener() {
+              @Override
+              public void onErrorResponse(VolleyError error) {
+                  Log.e("TAG", error.getMessage(), error);
+              }
+        }) {
+              @Override
+              protected HashMap<String, String> getParams()throws AuthFailureError {
+                  HashMap<String, String> hashMap = new HashMap<String, String>();
+                  hashMap.put("phone", PHONENUMBER_COMPLETE);
+                  hashMap.put("tag", tag);
+                  return hashMap;
+              }
+        };
+        mQueue.add(PostRequest);
     }
 
     public interface CallBack {
