35733e7ded6fcbfb0e84e81256c38d1ed7efbe7c
karon <karon@yahoo.com>
2015-05-25 19:44:04 +0800

    A1: fix server url .

--- src/com/a1os/cloud/phone/PhoneUtil.java
+++ src/com/a1os/cloud/phone/PhoneUtil.java
@@ -3,7 +3,6 @@ package com.a1os.cloud.phone;
 import java.util.HashMap;
 
 import com.a1os.cloud.request.APIRequest;
-import com.a1os.cloud.util.L1Cache;
 import com.a1os.cloud.util.L2Cache;
 import com.a1os.cloud.util.StringCache;
 
@@ -19,10 +18,9 @@ import android.util.Log;
 
 public final class PhoneUtil {
 
-    static String A1OS_API = "http://www.aoidone.com/api/cloud_data.php?number=";
-    static String A1OS_API2 = "http://www.aoidone.com/api/upload.php";
+    static String A1OS_API = "http://aoidone.com/api/cloud_data.php?number=";
+    static String UPLOAD_API = "http://aoidone.com/api/upload.php";
 
-    static L1Cache<String, String> mL1Cache = new L1Cache<String, String>(2 * 1024 * 1024);
     static StringCache mL2Cache;
 
     public static void getNumberInfo(Context ctx, final String phoneNumber, final CallBack callBack) {
@@ -30,10 +28,10 @@ public final class PhoneUtil {
         final String PHONENUMBER_COMPLETE = phoneNumber.replaceAll("(?:-| )", "");
         final String A1OS_URL = A1OS_API + PHONENUMBER_COMPLETE;
 
-        mL2Cache = new L2Cache(ctx, 10 * 1024 *1024);
+        mL2Cache = new L2Cache(ctx, 10 * 1024 * 1024);
 
-        if (mL1Cache.get(PHONENUMBER_COMPLETE) != null) {
-            callBack.execute(mL1Cache.get(PHONENUMBER_COMPLETE));
+        if (mL2Cache.get(PHONENUMBER_COMPLETE) != null) {
+            callBack.execute(mL2Cache.get(PHONENUMBER_COMPLETE));
             return;
         }
 
@@ -44,7 +42,6 @@ public final class PhoneUtil {
               @Override
               public void onResponse(String response) {
                   callBack.execute(response);
-            	  mL1Cache.put(PHONENUMBER_COMPLETE, response);
             	  mL2Cache.put(PHONENUMBER_COMPLETE, response);
               }
         }, new Response.ErrorListener() {
@@ -64,7 +61,7 @@ public final class PhoneUtil {
 
         RequestQueue mQueue = Volley.newRequestQueue(ctx);
 
-        APIRequest PostRequest = new APIRequest(Method.POST, A1OS_API2,
+        APIRequest PostRequest = new APIRequest(Method.POST, UPLOAD_API,
            new Response.Listener<String>() {
               @Override
               public void onResponse(String response) {
